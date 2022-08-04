ad3d15bcfa08846cee1fac4f5f124d950c20677b
itsmewhy <106473511+itsmewhy@users.noreply.github.com>
2022-08-04 15:27:01 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Whyzu-Userbot Telegram
+
+Whyzu-Userbot adalah userbot Telegram modular yang berjalan di Python3 dengan Library Pyrogram.
+
+Saya membuat repository ini untuk bersenang-senang sekaligus membantu anda mengelola grup secara efisien dan untuk membantu mengurangi kebosanan saat menggunakan telegram.
+
+## ⚠️ Disclaimer
+
+```
+Saya tidak bertanggung jawab atas penyalahgunaan bot ini.
+Gunakan bot ini dengan risiko Anda sendiri.
+Gunakan userbot ini dengan bijak.
+Ketika anda sudah memasang userbot ini, berarti anda sudah siap dengan resikonya.
+```
+
+<h3 align="center">Klik Tombol di Bawah ini untuk Deploy di Heroku</h3>
+<p align="center"><a href="https://dashboard.heroku.com/new?template=https://github.com/itsmewhy/Whyzu-Userbot"><img src="https://www.herokucdn.com/deploy/button.png" alt="Deploy to Heroku" target="_blank"/></a></p>
+
+## 🏷 Support
+
+- Follow Channel [@WhyzuCh](https://t.me/WhyzuCH) untuk info Update bot 
+- Gabung Group [@WhyzuNotSupport](https://t.me/WhyzuNotSupport) untuk diskusi, pelaporan bug, dan bantuan tentang Whyzu-Userbot.
+
+## 👨🏻‍💻 Credits
+-  [Why](https://github.com/itsmewhy)
---
+++ config.py
@@ -0,0 +1,53 @@
+# Credits: @Why
+# Copyright (C) 2022 Whyzu-ManUserbot
+#
+# This file is a part of < https://github.com/itsmewhy/Whyzu-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/itsmewhy/Whyzu-Userbot/blob/main/LICENSE/>.
+#
+# t.me/WhyzuNotSupport & t.me/WhyzuCH
+
+from base64 import b64decode
+from distutils.util import strtobool
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "☀️")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/f2adf77c7055a4e69ce60.jpg")
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
+API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", ""))
+BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
+if not BLACKLIST_CHAT:
+    BLACKLIST_CHAT = [-1001726983075]
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+BOT_VER = "0.1.0@main"
+BRANCH = getenv("BRANCH", "main")
+CHANNEL = getenv("CHANNEL", "WhyzuCH")
+CMD_HANDLER = getenv("CMD_HANDLER", ".")
+DB_URL = getenv("DATABASE_URL", "")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN",
+    b64decode("Z2hwXzg1Z1I1RlRRanRRTG9TU2FaRGVrcnI4c1pBSDB6cjRRMlN5UA==").decode(
+        "utf-8"
+    ),
+)
+GROUP = getenv("GROUP", "WhyzuNotSupport")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
+PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
+REPO_URL = getenv(
+    "REPO_URL",
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL3Byb2plY3RtNG4vUHlyb01hbg==").decode("utf-8"),
+)
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
