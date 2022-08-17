9f294f28768e9d380d1a77b947e47da27a106cb3
nathaell <110729662+nathxe@users.noreply.github.com>
2022-08-18 00:34:37 +0700

    Update help.py

--- WhyzuProject/modules/help.py
+++ WhyzuProject/modules/help.py
@@ -28,7 +28,7 @@ async def module_help(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "Whyzu-UserBot Modules"
+        ac.title = "PyroNath-UserBot Modules"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
@@ -43,7 +43,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @WhyzuCH"
+            this_command += "© @im_el69"
             await edit_or_reply(message, this_command, parse_mode="markdown")
         else:
             await edit_or_reply(
