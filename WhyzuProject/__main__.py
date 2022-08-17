a1ef5d9f5df431108be3e264b9e8d53bdd8e09db
nathaell <110729662+nathxe@users.noreply.github.com>
2022-08-18 00:53:18 +0700

    Update __main__.py

--- WhyzuProject/__main__.py
+++ WhyzuProject/__main__.py
@@ -1,11 +1,11 @@
 # Credits: @mrismanaziz
 # Copyright (C) 2022 Pyro-ManUserbot
 #
-# This file is a part of < https://github.com/mrismanaziz/Whyzu-Userbot/ >
+# This file is a part of < https://github.com/nathxe/PyroNath-Userbot/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/mrismanaziz/Whyzu-Userbot/blob/main/LICENSE/>.
+# <https://www.github.com/nathxe/PyroNath-Userbot/blob/main/LICENSE/>.
 #
-# t.me/SharingUserbot & t.me/Lunatic0de
+# t.me/nathsupport & t.me/nathaellxx
 
 import importlib
 
@@ -18,7 +18,7 @@ from WhyzuProject.helpers.misc import heroku
 from WhyzuProject.modules import ALL_MODULES
 
 MSG_ON = """
-🔥 **Whyzu-Userbot Berhasil Di Aktifkan**
+⚡ **PyroNath-Userbot Berhasil Di Aktifkan**
 ━━
 ➠ **Userbot Version -** `{}`
 ➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
@@ -33,21 +33,21 @@ async def main():
         try:
             await bot.start()
             bot.me = await bot.get_me()
-            await bot.join_chat("WhyzuCH")
-            await bot.join_chat("WhyzuRoom")
+            await bot.join_chat("nathaellxx")
+            await bot.join_chat("nathsupport")
             await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
             LOGGER("WhyzuProject").info(
                 f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
             )
         except Exception as a:
             LOGGER("main").warning(a)
-    LOGGER("WhyzuProject").info(f"Whyzu-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    LOGGER("WhyzuProject").info(f"PyroNath-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
     await idle()
     await aiosession.close()
 
 
 if __name__ == "__main__":
-    LOGGER("WhyzuProject").info("Starting Whyzu-UserBot")
+    LOGGER("WhyzuProject").info("Starting PyroNath-UserBot")
     install()
     heroku()
     LOOP.run_until_complete(main())
