7d3cfd5d357a45a43b223e3026ef2f6b607adcf7
aldiboyy <108301147+aldiboyy@users.noreply.github.com>
2022-09-03 18:46:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ load_dotenv("config.env")
 
 
 ALIVE_EMOJI = getenv("ALIVE_EMOJI", "☀️")
-ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/91444fd9ed03c2f90c2a5.jpg")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/7f9df89edcd749ac57b02.jpg")
 ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
 API_HASH = getenv("API_HASH")
 API_ID = int(getenv("API_ID", ""))
